70abf9187c1fe4a6f184bdcf9379f4f70b8663d3
jefferson <mirarajames29@gmail.com>
2020-07-17 08:22:36 +0300

    Add the templates folder and email.py file

---
+++ maps/email.py
@@ -0,0 +1,14 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    subject='Welcome to Riotcrime app'
+    sender=''
+    
+    #passing in the context variable
+    text_content=render_to_string('email/email.txt',{'name': name})
+    html_content=render_to_string('email/email.html',{'name':name})
+    
+    msg=EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
--- maps/forms.py
+++ maps/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profile,Riot
+
+class RegisterUser(UserCreationForm):
+    def __init__(self,*args,**kwargs):
+        super(RegisterUser,self).__init__(*args,**kwargs)
+        for fieldname in ['username','email','password1','password2']:
+            self.fields[fieldname].help_text=None
+            
+    email=forms.EmailField()
+    
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
 
 class UpdateUser(forms.ModelForm):
     email=forms.EmailField()

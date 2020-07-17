9690f110fe9412077fa43b0dc13bcbe14bcd70d0
jefferson <mirarajames29@gmail.com>
2020-07-17 08:29:56 +0300

    Add the changes made

--- maps/admin.py
+++ maps/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Riot
 # Register your models here.
+@admin.register(Riot)
+class RiotAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
